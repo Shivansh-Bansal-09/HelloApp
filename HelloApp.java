c882e7af7efb3019ede1062a5d43e74f677b3509
Shivansh-Bansal-09 <shivansh8c@gmail.com>
2026-03-17 08:57:48 +0530

    UC5: Multiple line command using enchaned for loop or default message

--- HelloApp.java
+++ HelloApp.java
@@ -1,32 +1,26 @@
-/**
- * HelloApp UC4 - Greets multiple users using command-line arguments
- * If no names are provided, it greets "World".
- */
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Default name
-        String name = "World";
-
-        // Check if names are provided
-        if (args.length > 0) {
-
+        // If no command-line arguments are given
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
             StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
 
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-
-                if (i < args.length - 1) {
+            // Enhanced for loop to read all arguments
+            for (String name : args) {
+                if (!first) {
                     nameBuilder.append(", ");
                 }
+                nameBuilder.append(name);
+                first = false;
             }
 
-            name = nameBuilder.toString();
+            // Display greeting
+            System.out.println("Hello, " + nameBuilder.toString() + "!");
         }
-
-        // Print greeting
-        System.out.println("Hello, " + name + "!");
     }
 }
